77a7eef20e7183f671488b939f40cb99aea99a6d
Jianyu Li <jianyu.li@feisystems.com>
2017-06-05 15:15:47 +0800

    update
    update

--- db.js
+++ db.js
@@ -8,7 +8,7 @@ var config = {
   user: 'lijianyu', //env var: PGUSER
   database: 'helloworld', //env var: PGDATABASE
   password: 'li19850124', //env var: PGPASSWORD
-  host: '45.76.66.37', // Server hosting the postgres database
+  host: '127.0.0.1', // Server hosting the postgres database
   port: 5432, //env var: PGPORT
   max: 10, // max number of clients in the pool
   idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
